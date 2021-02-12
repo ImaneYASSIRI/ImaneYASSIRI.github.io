08138b9693dad45d49a4a5bd32a4c40c0fd8909d
ImaneYASSIRI <56323846+ImaneYASSIRI@users.noreply.github.com>
2021-02-13 00:24:51 +0100

    Add files via upload

---
+++ src/chart.js
@@ -0,0 +1,129 @@
+//using chart.js
+var updateChartInterval;
+var elapsedDays = 0;
+var COLORS = {
+    circles: {
+        susceptible: {
+            fill: '#FFFFFF',
+            stroke: 'blue',
+        },
+        infected: {
+            fill: '#FF0000',
+            stroke: 'red',
+        },
+        recovered: {
+            fill: '#00FFE7',
+            stroke: 'green',
+        },
+        dead: {
+            fill: '',
+            stroke: 'gray',
+        }
+    },
+}
+
+var config = {
+    type: 'line',
+    data: {
+        labels: [],
+        datasets: [{
+            label: 'Susceptible',
+            borderColor: COLORS.circles.susceptible.stroke,
+            data: [],
+            fill: '-1',
+        }, {
+            label: 'Rétablis',
+            fill: 'end',
+            borderColor: COLORS.circles.recovered.stroke,
+            data: [],
+            fill: '-1',
+        }, {
+            label: 'Infectés',
+            fill: 'end',
+            borderColor: COLORS.circles.infected.stroke,
+            data: [],
+            fill: '-1',
+        },
+        {
+            label: 'Morts',
+            fill: 'end',
+            borderColor: COLORS.circles.dead.stroke,
+            data: [],
+            fill: '-1',
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        tooltips: {
+            mode: 'index',
+            intersect: false,
+        },
+        hover: {
+            mode: 'nearest',
+            intersect: true
+        },
+        scales: {
+            xAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Jours'
+                }
+            }],
+            yAxes: [{
+                display: true,
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Nombre des individus'
+                },
+                ticks: {
+                    suggestedMax: init_population,
+                },
+                stacked: false
+            }]
+        },
+        annotation: {
+          annotations: []
+        }
+    }
+};
+
+
+
+
+window.onload = function () {
+    var ctx = document.getElementById('chart').getContext('2d');
+    lineGraph = new Chart(ctx, config);
+};
+
+function resetChart() {
+    config.data.labels = [];
+    config.data.datasets[0].data = [];
+    config.data.datasets[1].data = [];
+    config.data.datasets[2].data = [];
+    config.data.datasets[3].data = [];
+    elapsedDays = 0;
+    if (updateChartInterval) {
+        clearInterval(updateChartInterval);
+    }
+    updateChart();
+}
+
+function pushData(total_healthy, total_infected, total_cured, total_deaths) {
+    config.data.labels.push(elapsedDays++);
+    config.data.datasets[0].data.push(total_healthy);
+    config.data.datasets[1].data.push(total_cured);
+    config.data.datasets[2].data.push(total_infected);
+    config.data.datasets[3].data.push(total_deaths);
+
+    lineGraph.update();
+}
+
+function updateChart() {
+    pushData(statistics.currentHealthy, statistics.currentInfected, statistics.currentRecovered, statistics.deaths);
+
+    if (statistics.currentInfected === 0) {
+        clearInterval(updateChartInterval);
+    }
+}
---
+++ src/parameters.js
@@ -0,0 +1,149 @@
+
+var chance_to_transmit = 14; //chance to transmit 
+var time_to_recover = 14; //time to recover
+var time_counter = 0;
+var init_infected=1; //initaial infected number
+var init_population=50;
+var runningSimulation=false;
+var speed = 4;
+var MAX_INITIAL_POPULATION=300;
+var R0;
+
+//social distancing vars
+var sd_factor = 0;
+var social_distancing = false;
+//isolation var
+var isolate_infected = false;
+goHome=false;
+var rdm = [];
+
+statistics.totalPeople = init_population;
+statistics.currentInfected = init_infected;
+statistics.currentRecovered = 0;
+statistics.deaths = 0;
+
+//Dashboard controls :
+//Initial population
+document.getElementById('initial_population').addEventListener('change', function (e) {
+    this.value = parseInt(this.value);
+    inputInitialPopulation = this.value;
+    if (inputInitialPopulation > MAX_INITIAL_POPULATION) {
+        this.value = init_population;
+        alert('The number is too high. Maximum value for initial population is ' + MAX_INITIAL_POPULATION);
+    } else {
+        init_population = inputInitialPopulation;
+        statistics.totalPeople = init_population;
+    
+        lineGraph.options.scales.yAxes[0].ticks.max = parseFloat(init_population);
+        lineGraph.update();
+    }
+
+    p1 =(75*init_population)/100;
+    
+    turns2=Math.round(p1)
+    for(j=0; j<p1; j++){
+        rdm.push(randomIntFromRange(0,init_population-1));
+    }
+    console.log(rdm);
+});
+
+document.getElementById('initial_population').value = init_population;
+
+//Initial Infected
+document.getElementById('initial_infected').addEventListener('change', function (e) {
+    inputInitialInfected = parseInt(this.value);
+    if (inputInitialInfected > init_population) {
+        alert('Number of infected must be lower than the number of total people');
+        this.value = initialInfected;
+    } else {
+        init_infected = inputInitialInfected;
+    }
+
+});
+
+document.getElementById('initial_infected').value = init_infected;
+
+// InfectioN rate or chance to transmit
+document.getElementById('infection_rate').addEventListener('input', function (e) {
+    chance_to_transmit = this.value;
+    document.getElementById('infection_rate_text').innerHTML = Math.round(chance_to_transmit);
+    R0 = (chance_to_transmit/100)/(1/time_to_recover);
+    document.getElementById("R0").innerHTML =  "R0  ≈ " + number_format(R0,2);
+});
+
+document.getElementById('infection_rate').value = chance_to_transmit;
+document.getElementById('infection_rate_text').innerText = chance_to_transmit;
+
+//Time to recover
+document.getElementById('time_to_recover').addEventListener('change', function (e) {
+    inputTimeToRecover = parseInt(this.value);
+    time_to_recover = inputTimeToRecover;
+    R0 = (chance_to_transmit/100)/(1/time_to_recover);
+    document.getElementById("R0").innerHTML =  "R0  ≈ " + number_format(R0,2);
+});
+
+document.getElementById('time_to_recover').value = time_to_recover;
+
+
+// social distancing 
+document.getElementById('social_distancing').addEventListener('input', function (e) {
+    social_distancing = this.checked;
+    console.log('distancing',social_distancing);
+    chance_to_transmit = 4;
+    document.getElementById('infection_rate').value = chance_to_transmit;
+    document.getElementById('infection_rate_text').innerText = chance_to_transmit;
+    R0 = (chance_to_transmit/100)/(1/time_to_recover);
+    document.getElementById("R0").innerHTML =  "R0  ≈ " + number_format(R0,2);
+});
+
+
+document.getElementById('social_distancing').checked = social_distancing;
+
+//Isolation
+document.getElementById('isolate_infected').addEventListener('input', function (e) {
+    isolate_infected = this.checked;
+    console.log('isolation',isolate_infected);
+});
+
+document.getElementById('isolate_infected').checked = isolate_infected;
+
+//lockdown
+document.getElementById('go_home').addEventListener('input', function (e) {
+    goHome = this.checked;
+    console.log('go home is active',goHome);
+});
+
+document.getElementById('go_home').checked = goHome;
+
+
+
+
+//Buttons
+document.getElementById('control_start').addEventListener('click', function (e) {
+    if (runningSimulation==false) {
+        init()
+        animate();
+        updateChart();
+        updateChartInterval = setInterval(updateChart, 1000);
+        runningSimulation=true;
+    } else {
+        stopSimulation();
+        resetChart()
+        init()
+        //animate();
+        updateChart();
+        updateChartInterval = setInterval(updateChart, 1000);
+    }
+    
+});
+
+document.getElementById('control_stop').addEventListener('click', function (e) {
+    stopSimulation();
+    resetChart();
+});
+
+
+let alertR0 = document.getElementById('alert-r0');
+if(R0<0.7) {
+    document.getElementById('alert-r0').setAttribute('class', 'alert alert-primary');
+}
